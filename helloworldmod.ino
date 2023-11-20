651996eed53a1bc714a4d26153c37a31e752a638
Acedia <110048632+SlothyAcedia@users.noreply.github.com>
2023-11-21 01:56:09 +0800

    Update helloworldmod.ino

--- helloworldmod.ino
+++ helloworldmod.ino
@@ -1,12 +1,12 @@
 void setup() {
   // initialize digital pin LED_BUILTIN as an output.
-  pinMode(9, OUTPUT);
+  pinMode(10, OUTPUT);
 }
 
 // the loop function runs over and over again forever
 void loop() {
-  digitalWrite(9, HIGH);  // turn the LED at Pin 9 on (HIGH is the voltage level)
-  delay(500);                      // wait for 500ms
-  digitalWrite(9, LOW);   // turn the LED at Pin 9 off by making the voltage LOW
-  delay(500);                      // wait for 500ms
+  digitalWrite(10, HIGH);  // turn the LED at Pin 9 on (HIGH is the voltage level)
+  delay(250);                      // wait for 250ms
+  digitalWrite(10, LOW);   // turn the LED at Pin 9 off by making the voltage LOW
+  delay(250);                      // wait for 250ms
 }
